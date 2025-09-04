39a4db01489ce429995261251389f47657e6551a
t-izumii <120708670+t-izumii@users.noreply.github.com>
2025-09-04 23:00:09 +0900

    update

--- study-31/src/webgl/glsl/fragment.glsl
+++ study-31/src/webgl/glsl/fragment.glsl
@@ -14,15 +14,12 @@ float random(vec2 p) {
 void main() {
   vec2 uv = vUv;
 
-  float div = 16.0;
-  vec2 duv = floor(uv * div) / div;
-
   float p = pow(uv.y, 2.0);
   float a1 = clamp((uProgress * 2.0 - p), 0.0, 1.0);
 
   // ランダム値を生成
   float r1 = random(gl_FragCoord.xy);
-  float r2 = random(gl_FragCoord.xy + vec2(1000.0));
+  float r2 = random(gl_FragCoord.xy + vec2(10.0));
 
   // 極座標→直交座標
   float ax = r1 * cos(r2 * 2.0 * PI);
@@ -46,3 +43,21 @@ void main() {
   result.a *= a1;
   gl_FragColor = result;
 }
+
+  // float p = pow(uv.y, 2.0);
+  // float a1 = clamp((uProgress * 2.0 - p), 0.0, 1.0); ←0~1を返す
+
+  // float r1 = random(gl_FragCoord.xy);
+  // float r2 = random(gl_FragCoord.xy + vec2(1000.0)); ←ピクセルごとにランダム値
+
+  // float ax = r1 * cos(r2 * 2.0 * PI);
+  // float ay = r1 * sin(r2 * 2.0 * PI); ←　ランダム値を半径にして直交座標を生成
+
+  // float r = texture2D(uTexture, uv + vec2(0.0, 0.05 * (1.0 - a1))).r;
+  // float g = texture2D(uTexture, uv).g;
+  // float b = texture2D(uTexture, uv + vec2(0.0, -0.05 * (1.0 - a1))).b;
+  // vec4 color = vec4(r, g, b, 1.0); ←A1の値によって色を変更
+
+  //　float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114)); ←内積（ドット積）でグレースケール
+
+  //　vec4 result = mix(vec4(vec3(gray), 1.0), color, pow(a1, 3.0)); ←　a1の値でカラーブレンド
--- study-31/src/webgl/glsl/vertex.glsl
+++ study-31/src/webgl/glsl/vertex.glsl
@@ -6,6 +6,23 @@ void main() {
   vUv = uv;
   vec3 pos = position;
 
-  pos.z += sin(-pos.y * 0.005 + uProgress * 3.14159) * 8.0 * (1.0 - abs(uProgress - 0.5) * 2.0);
+  pos.z += sin(-pos.y * 0.005 + uProgress * 3.14159) * (1.0 - abs(uProgress - 0.5) * 2.0) * 8.0;
   gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
+
+// pos.z += sin(-pos.y * 0.005 + uProgress * 3.14159) * (1.0 - abs(uProgress - 0.5) * 2.0) * 8.0;
+
+// uProgress = 0.0 → sin(-pos.y * 0.005 + 0)     = 元の波
+// uProgress = 0.5 → sin(-pos.y * 0.005 + 1.57)  = 波が90度ずれる
+// uProgress = 1.0 → sin(-pos.y * 0.005 + 3.14)  = 波が180度ずれる（反転）
+
+// πは180度
+
+// 時間 0.0: sin(0 + 0度)   = sin(0)   = 0
+// 時間 0.5: sin(0 + 90度)  = sin(π/2) = 1    ← 最大値
+// 時間 1.0: sin(0 + 180度) = sin(π)   = 0
+
+//　(1.0 - abs(uProgress - 0.5) * 2.0) * 8.0
+// uProgress = 0.0 → 1.0 - 1.0 = 0.0  ← 効果なし
+// uProgress = 0.5 → 1.0 - 0.0 = 1.0  ← 最大効果
+// uProgress = 1.0 → 1.0 - 1.0 = 0.0  ← 効果なし
